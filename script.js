bb34ba1ce5aa5e3fc6fc2bff27bb50261644fe8e
Suvo Ghosh <suvosh663@gmail.com>
2024-12-27 19:45:14 +0530

    Fixed some issue

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const textArea = document.querySelector('textarea')
 const btn = document.querySelector('button')
 
-
-btn.addEventListener('click', ()=>{  
+btn.addEventListener('click', () => {
     let speech = new SpeechSynthesisUtterance(textArea.value)
-    let voices = window.speechSynthesis.getVoices(); 
-    speech.voice = voices[Math.floor(Math.random()*voices.length)]
+    let voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[Math.floor(Math.random() * voices.length)]
     window.speechSynthesis.speak(speech)
 })
